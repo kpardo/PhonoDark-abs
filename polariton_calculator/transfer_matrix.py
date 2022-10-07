b093380a30ecff17d1aff389d640915c33748de7
Kris Pardo <kpardo@caltech.edu>
2022-10-07 09:29:22 -0700

    started h matrix coding...not sure if correct org.

--- polariton_calculator/transfer_matrix.py
+++ polariton_calculator/transfer_matrix.py
@@ -36,13 +36,12 @@ class TransferMatrix:
         KgK_mat = np.matmul(K_mat, np.matmul(g_mat, K_dag))
 
         eigen_vals, eigen_vecs = sla.eig(KgK_mat)
-        # create dict of eigen vectors and values
-        eigen_dict = {(v, i):vec for i,(v,vec) in enumerate(zip(eigen_vals, eigen_vecs))}
-        # sort Umat by eigenvalues
+        # sort Umat by eigenvalues, descending order
+        ## FIXME: Negative eigenvalues should be in ascending order...
         U_mat = np.array(eigen_vecs[np.argsort(eigen_vals)[::-1]], dtype=complex)
-
         U_mat_dag = np.conj(U_mat.T)
 
+        ## below is good unit test later.
         if not np.allclose(np.matmul(U_mat_dag, U_mat), np.identity(h_mat_dim)):
             print('Eigenvectors of h are NOT orthonormalized.')
 
@@ -60,10 +59,47 @@ class TransferMatrix:
 class TMatPol(TransferMatrix):
     '''
     Calculates the T matrix for phonon-polariton contributions.
-    Inputs: h matrix
+    Inputs: ??
     Outputs: diagonalized T matrix
     '''
-    pass
+    qvec: np.ndarray
+    dielectric: np.ndarray
+    o_phon_energy = np.ndarray
+
+    def __post_init__():
+        self.h = self.get_h_matrix()
+
+
+    def get_n_modes(self, o_phon_energy):
+        num_phon_modes = len(o_phon_energy) + 3 ## tot num of phonons
+        num_phot_modes = len(o_phon_energy)
+        num_o_phot_modes = 2 ## number of photon modes
+        num_pol_modes = num_phot_modes + num_o_phon
+        return num_phon_modes, num_phot_modes, num_o_phot_modes, num_pol_modes
+
+    def split_energy(energies, split_amount):
+    	"""
+
+    	Splits the energy levels by the split_amount.
+        Removes degeneracy in eigenvalues
+
+    	"""
+    	n_energies = len(energies)
+    	for i in range(n_energies):
+    		split_amount = split*(i - n_energies/2)
+    		energies[i] += split_amount
+    	return energies
+
+    def get_h_matrix(self, split_en_level = 10**(-8)):
+        modes =self.get_n_modes(self.o_phon_energy)
+        num_phon_modes, num_phot_modes, num_o_phot_modes, num_pol_modes = modes
+
+        ## set up h matrix shape & container
+        h_mat_dim = 2*num_pol_modes
+        h_mat = np.zeros((h_mat_dim, h_mat_dim), dtype=complex)
+
+        ## split energies by a bit to remove eigenvalue degeneracies
+        pass
 
 @dataclass
 class TMatPhonon(TransferMatrix):
