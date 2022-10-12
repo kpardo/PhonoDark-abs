ab92023b54798437954a2ef81aeede3e4a5fcf46
Kris Pardo <kpardo@caltech.edu>
2022-10-11 17:29:52 -0700

    actually fixed eigenvector sort now.

--- polariton_calculator/transfer_matrix.py
+++ polariton_calculator/transfer_matrix.py
@@ -7,6 +7,7 @@ from dataclasses import dataclass
 import numpy as np
 from scipy import linalg as sla
 import physics
+import sys
 
 @dataclass
 class TransferMatrix:
@@ -18,11 +19,17 @@ class TransferMatrix:
     hmatrix: np.ndarray
 
     def __post_init__(self):
+        '''
+        gets E and T matrix as part of init.
+        '''
         self.E_mat, self.T_mat = self.get_T_matrix()
 
     def get_T_matrix(self):
+        '''
+        gets the E and T matrices according to
+        2005.10256 Appendix C algorithm
+        '''
         h_mat_dim = len(self.hmatrix)
-        num_o_phon = h_mat_dim//2 - 2
 
         g_mat_diag = np.ones(h_mat_dim//2)
         g_mat = np.zeros((h_mat_dim, h_mat_dim))
@@ -42,12 +49,11 @@ class TransferMatrix:
 
         ## first sort by descending order
         sort_index = np.argsort(eigen_vals)[::-1]
-        ## find index of first negative number in sort
-        arg_first_neg = (np.less(eigen_vals[sort_index], 0)).argmax()
-        ## flip negative part of indexes
-        sort_index[arg_first_neg:] = np.flip(sort_index[arg_first_neg:])
-        ## get sort eigen vecs according to sorted eigen vals.
-        U_mat = np.array(eigen_vecs[sort_index], dtype=complex)
+        ## flip second half of eigenvalues
+        sort_index[h_mat_dim//2:] = np.flip(sort_index[h_mat_dim//2:])
+        ## get sorted eigen vecs according to sorted eigen vals.
+        U_mat = np.array(eigen_vecs[:,sort_index], dtype=complex)
+
         U_mat_dag = np.conj(U_mat.T)
 
         ## below is good unit test later.
@@ -58,12 +64,14 @@ class TransferMatrix:
 
         E_mat = np.matmul(g_mat, L_mat)
 
-        T_mat = np.matmul(K_mat_inv, np.matmul(U_mat, sla.sqrtm(E_mat)))
+        sqrt_E_mat = sla.sqrtm(E_mat)
+
+        T_mat = np.matmul(K_mat_inv, np.matmul(U_mat, sqrt_E_mat))
 
         return [E_mat, T_mat]
 
 @dataclass
-class TMatPol(TransferMatrix):
+class TMatPol():
     '''
     Calculates the T matrix for phonon-polariton contributions.
     Inputs: ??
@@ -84,21 +92,18 @@ class TMatPol(TransferMatrix):
         num_pol_modes = num_phot_modes + num_o_phon
         return num_phon_modes, num_phot_modes, num_o_phot_modes, num_pol_modes
 
-    def split_energy(energies, split_amount):
-    	"""
-
-    	Splits the energy levels by the split_amount.
+    @staticmethod
+    def split_energy(energies, split):
+        """
+        Splits the energy levels by the split amount.
         Removes degeneracy in eigenvalues
-
-    	"""
-    	n_energies = len(energies)
-    	for i in range(n_energies):
-    		split_amount = split*(i - n_energies/2)
-    		energies[i] += split_amount
-    	return energies
+        """
+        energies = [e + split * (i - len(energies) / 2)
+         for i,e in enumerate(energies)]
+        return energies
 
     def get_h_matrix(self, split_en_level = 10**(-8)):
-        modes =self.get_n_modes(self.o_phon_energy)
+        modes = self.get_n_modes(self.o_phon_energy)
         num_phon_modes, num_phot_modes, num_o_phot_modes, num_pol_modes = modes
 
         ## set up h matrix shape & container
@@ -106,6 +111,18 @@ class TMatPol(TransferMatrix):
         h_mat = np.zeros((h_mat_dim, h_mat_dim), dtype=complex)
 
         ## split energies by a bit to remove eigenvalue degeneracies
+        o_phon_energy = split_energy(o_phon_energy, split_en_level)
+        phot_energy = np.real(split_energy(phot_energy, split_en_level))
+
+        ## term I: free phonon
+        h_mat[:num_o_phon, :num_o_phon] = 0.5*np.diag(o_phon_energy)
+        h_mat[num_pol_modes:h_mat_dim-2, num_pol_modes:h_mat_dim-2] = 0.5*np.diag(o_phon_energy)
+
+        # term II: free photon
+        h_mat[num_o_phon:h_mat_dim//2, num_o_phon:h_mat_dim//2] += 0.5*np.diag(phot_energy)
+        h_mat[h_mat_dim-2:h_mat_dim, h_mat_dim-2:h_mat_dim] += 0.5*np.diag(phot_energy)
+
+
         pass
 
 @dataclass
