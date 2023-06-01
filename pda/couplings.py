fixed dark photon normalization

pda/couplings.py
@@ -83,7 +83,7 @@ class DarkPhoton:
     texop: str = r'$\kappa e \phi_\mu\bar\psi \gamma^\mu\psi$'
     texcoupconst: str = r'$\kappa$'
     formfac: np.ndarray = np.zeros((1))
-    prefac: np.float64 = 1.  # e's taken from conversion from g to kappa
+    prefac: np.float64 = 1. # e's cancel from conversion from g to kappa
     se_shape: str = 'vector'
 
     def __post_init__(self):

pda/reach.py
@@ -22,4 +22,6 @@ def reach(mass_list, q_XYZ_list, mat, coupling=None, snr_cut=3,
     rate = r.rate(mass_list, q_XYZ_list, mat,
                   coupling=coupling, pol_mixing=pol_mixing)
     reach = np.sqrt(snr_cut / (rate * exposure))
-    return (reach*(u.eV)**(-1)).to(1./u.GeV)
+    # return (reach*(u.eV)**(-1)).to(1./u.GeV)
+    # reach is actually dimensionless!!
+    return reach