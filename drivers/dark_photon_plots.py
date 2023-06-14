e36862f16f42e1cc51e6ce69b5d151c0f10dd89b
Kris Pardo <kpardo@caltech.edu>
2023-06-13 17:24:47 -0700

    dark photon fig updates.

--- drivers/dark_photon_plots.py
+++ drivers/dark_photon_plots.py
@@ -46,7 +46,7 @@ al_kappa = pd.read_csv('../data/limit_data/kappa/al2o3_kappa.dat', names=['mv', 
 ff = pd.read_csv('../data/limit_data/AxionLimits/FifthForce_BL.txt', skiprows=3, delimiter='\s+', names=['mass [eV]', 'g_BL'])
 
 # FIXME: move to plotting script
-def plot_coupling(coupling, ax=None, legend=True, mo=False):
+def plot_coupling(coupling, ax=None, legend=True, mo=False, data=False):
     if ax==None:
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1)
@@ -62,6 +62,8 @@ def plot_coupling(coupling, ax=None, legend=True, mo=False):
         print(np.min(reach))
         ax.loglog(mlist*1000, reach, color=cs[i], label=f'$\mathrm{{{mat.name}}}$', lw=2)
     if legend:
+        if data:
+            ax.loglog(1,1, ls='dashed', color='black', label=r'$\mathrm{Data}$')
         ax.legend(fontsize=16, loc='lower right')
     if ax==None:
         fig.savefig(f'../results/plots/{coupling.name}.pdf')
@@ -69,41 +71,63 @@ def plot_coupling(coupling, ax=None, legend=True, mo=False):
         return ax
 
 ## start plotting -- steal some of Tanner's formatting
-ncols = 2
+## plot each separately
+## plot kinetic mixing first
+ncols = 1
+nrows = 1
+f, ax = plt.subplots(nrows=nrows, ncols=ncols,
+                        figsize=(7*1.1*ncols, 7*nrows))
+
+ax.set_ylabel(r'$\kappa$')
+ax.set_xlabel(r'$m_V~[\rm{meV}]$')
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.minorticks_on()
+ax.set_xlim([10, 1000])
+ax.set_ylim([1.e-18, 1.e-14])
+
+## plot our constraints
+co = coup.DarkPhoton(q_XYZ_list=qs, omega=mlist)
+plot_coupling(co, ax=ax, legend=True, data=True)
+ax.plot((ga_kappa['mv'].to_numpy()*u.eV).value*1000, ga_kappa['kappa'], ls='dashed', c=cs[0])
+ax.plot((al_kappa['mv'].to_numpy()*u.eV).value*1000, al_kappa['kappa'], ls='dashed', c=cs[1])
+ax.plot((si_kappa['mv'].to_numpy()*u.eV).value*1000, si_kappa['kappa'], ls='dashed', c=cs[2])
+
+
+
+## save fig.
+f.tight_layout()
+f.savefig('../results/plots/dark_photon_kineticmixing_fig.pdf')
+
+## plot g_B-L
+ncols = 1
 nrows = 1
 f, ax = plt.subplots(nrows=nrows, ncols=ncols,
                         figsize=(7*1.1*ncols, 7*nrows))
 
 y_labels = [ r'$\kappa$', r'$g_{B-L}$']
-[axx.set_ylabel(y) for axx,y in zip(ax, y_labels)]
-[axx.set_xlabel(r'$m_V~[\rm{meV}]$') for axx in ax]
-[axx.set_xscale('log') for axx in ax]
-[axx.set_yscale('log') for axx in ax]
-[axx.minorticks_on() for axx in ax]
-[axx.set_xlim([10, 1000]) for axx in ax]
-ax[0].set_ylim([1.e-18, 1.e-14])
-ax[1].set_ylim([1.e-20, 1.e-12])
+ax.set_ylabel(r'$g_{B-L}$')
+ax.set_xlabel(r'$m_V~[\rm{meV}]$')
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.minorticks_on()
+ax.set_xlim([10, 1000])
+ax.set_ylim([1.e-18, 1.e-12])
 
 ## plot our constraints
-co = coup.DarkPhoton(q_XYZ_list=qs, omega=mlist)
-plot_coupling(co, ax=ax[0], legend=True)
-
 co = coup.BminusL(q_XYZ_list=qs, omega=mlist)
-plot_coupling(co, ax=ax[1], legend=False)
+plot_coupling(co, ax=ax, legend=True)
 
 ## plot other limits
-ax[0].plot((ga_kappa['mv'].to_numpy()*u.eV).value*1000, ga_kappa['kappa'], ls='dashed', c=cs[0])
-ax[0].plot((al_kappa['mv'].to_numpy()*u.eV).value*1000, al_kappa['kappa'], ls='dashed', c=cs[1])
-ax[0].plot((si_kappa['mv'].to_numpy()*u.eV).value*1000, si_kappa['kappa'], ls='dashed', c=cs[2])
 
-ax[1].plot(ff['mass [eV]'].to_numpy()*1000, ff['g_BL'].to_numpy(), c=cs[5])
-ax[1].fill_between(ff['mass [eV]'].to_numpy()*1000, 
+ax.plot(ff['mass [eV]'].to_numpy()*1000, ff['g_BL'].to_numpy(), c=cs[5])
+ax.fill_between(ff['mass [eV]'].to_numpy()*1000, 
 ff['g_BL'].to_numpy(), 
 1.e-10*np.ones(len(ff)), color=cs[5], alpha=0.3
 )
-ax[1].text(10**1.05, 10**-16.5, r'$\mathrm{Fifth} \; \mathrm{Force}$',
-            rotation = 35, fontsize = 30, color = cs[5])
+ax.text(10**1.05, 3*10**-13, r'$\mathrm{Fifth} \; \mathrm{Force}$',
+            rotation = 0, fontsize = 30, color = cs[5])
 
 ## save fig.
 f.tight_layout()
-f.savefig('../results/plots/dark_photon_fig.pdf')
+f.savefig('../results/plots/dark_photon_gBL_fig.pdf')
